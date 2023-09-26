90c260ca58b8a71631c64445268a5e308c3d1349
LeonardoT-V <iderltv12@gmail.com>
2023-09-25 19:47:53 -0500

    08 - estructura del for en go

---
+++ control/condFor.go
@@ -0,0 +1,18 @@
+package control
+
+import "fmt"
+
+func BasicForStuct() {
+	/* for i := 0; i <= 100; i += 10 {
+		fmt.Println(i)
+	} */
+	/* for i := 0; i <= 10; {
+		fmt.Println(i)
+		i++
+	} */
+	i := 0
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/leonardoT-V/go-notes/teclado"
+	"github.com/leonardoT-V/go-notes/control"
 )
 
 func main() {
-	teclado.InputKeyboardNumber()
+	control.BasicForStuct()
 }
