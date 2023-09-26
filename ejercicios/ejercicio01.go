bdd03c5632a503089435feb2429bb030c91c92e1
LeonardoT-V <iderltv12@gmail.com>
2023-09-25 19:13:43 -0500

    06[3] - ejercicio numero es mayor a 100

--- ejercicios/ejercicio01.go
+++ ejercicios/ejercicio01.go
@@ -6,14 +6,9 @@ import (
 )
 
 func Ejercicio1(s string) (int, string) {
-	num, _ := strconv.Atoi(s)
-	var textToSend string
-	if num >= 100 {
-		textToSend = fmt.Sprintf("El numero %v es mayor a 100", num)
-		return num, textToSend
+	if num, _ := strconv.Atoi(s); num >= 100 {
+		return num, fmt.Sprintf("El numero %v es mayor a 100", num)
 	} else {
-		textToSend = fmt.Sprintf("El numero %v es menor a 100", num)
-		return num, textToSend
+		return num, fmt.Sprintf("El numero %v es menor a 100", num)
 	}
-
 }
